20088c253dbaeba627c5eb242563b71d056d2cff
mthiagoalves <m.thiago.ales@live.com>
2025-10-26 20:48:00 -0300

    feat(Homepage): create homepage

---
+++ resources/js/pages/Homepage.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { dashboard, login, register } from '@/routes';
+import { Head, Link } from '@inertiajs/vue3';
+import AppLayout from '@/layouts/AppLayout.vue';
+withDefaults(
+    defineProps<{
+        canRegister: boolean;
+    }>(),
+    {
+        canRegister: true,
+    },
+);
+</script>
+
+<template>
+
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <AppLayout/>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('Homepage', [
         'canRegister' => Features::enabled(Features::registration()),
     ]);
 })->name('home');
