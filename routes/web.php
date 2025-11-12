4dcbfa4b2b108b4276c89601366048c7c9e37b40
mthiagoalves <m.thiago.ales@live.com>
2025-11-12 08:58:51 -0300

    feat(web): create new routes products

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -8,8 +9,15 @@ Route::get('/', function () {
     return Inertia::render('Homepage');
 })->name('home');
 
+Route::get('/all-products', [ProductController::class, 'allProducts'])->name('products.index');
+
+Route::get('/new-products', [ProductController::class, 'newProducts'])->name('products.new');
+
+Route::get('/category/{slug}', [ProductController::class, 'category'])->name('products.category');
+
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
+
+require __DIR__ . '/settings.php';
