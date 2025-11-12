2e6f3e39f32915efe99ded5c3951cc7cc62db35c
mthiagoalves <m.thiago.ales@live.com>
2025-11-12 10:15:24 -0300

    feat(AppLayout): apply static components, worked in all website

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
-import Header from '@/components/used-components/Header.vue';
 import Footer from '@/components/used-components/Footer.vue';
-import AppLayout from '@/layouts/app/AppSidebarLayout.vue';
+import Header from '@/components/used-components/Header.vue';
+import { useGeneralModal } from '@/composables/useGeneralModal';
+import GeneralModal from '@/components/used-components/includes/modals/GeneralModal.vue';
+import { useSearchModal } from '@/composables/useSearchModal';
+import SearchModal from '@/components/used-components/includes/modals/SearchModal.vue';
 import type { BreadcrumbItemType } from '@/types';
 
 interface Props {
@@ -11,14 +14,19 @@ interface Props {
 withDefaults(defineProps<Props>(), {
     breadcrumbs: () => [],
 });
+
+const { isOpen } = useGeneralModal();
+const { isOpen: isSearchOpen } = useSearchModal();
 </script>
 
 <template>
     <div class="min-h-screen bg-gray-50 text-gray-900">
         <Header />
-        <main class="mx-auto max-w-7xl p-6">
+        <main class="max-w-full">
             <slot />
         </main>
+        <GeneralModal v-if="isOpen" />
+        <SearchModal v-if="isSearchOpen" />
         <Footer />
     </div>
 </template>
