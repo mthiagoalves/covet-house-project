832b16e0bb8ea2cf4814c7325e000f8909fcd725
mthiagoalves <m.thiago.ales@live.com>
2025-11-20 20:34:46 -0300

    style(Footer): adjust mobile padding in formHomepage component

--- resources/js/components/used-components/Footer.vue
+++ resources/js/components/used-components/Footer.vue
@@ -7,13 +7,19 @@ import BrandsMarquee from './includes/footer/BrandsMarquee.vue';
 import AppFooter from './includes/footer/AppFooter.vue';
 import ScrollToTopButton from './includes/footer/ScrollToTopButton.vue';
 import SocialSidebar from './includes/footer/SocialSidebar.vue';
+import { usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
 
+
+const page = usePage();
+
+const isHomepage = computed(() => page.url === '/');
 </script>
 
 <template>
     <RoomSlider class="pb-[4px]" />
-    <FormHomepage />
-    <IdServiceSection class="pt-[6px]" />
+    <FormHomepage v-if="isHomepage" />
+    <IdServiceSection :class="isHomepage ? 'pt-[6px]' : 'pt-[2px]'" />
     <EbookSlider />
     <BrandsMarquee />
     <AppFooter />
