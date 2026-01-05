b65744288454c573a2dcf6ce2cb6883db5312d02
mthiagoalves <m.thiago.ales@live.com>
2026-01-05 07:20:35 -0300

    feat(bootstrap.app): make new function to redirect to page errors.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,11 +6,14 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -23,5 +26,23 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (Throwable $e, Request $request) {
+
+            if ($e instanceof NotFoundHttpException && !$request->wantsJson()) {
+                return Inertia::render('Error', ['status' => 404])
+                    ->toResponse($request)
+                    ->setStatusCode(404);
+            }
+
+            $response = parent::render($request, $e);
+            $status = $response->getStatusCode();
+
+            if (!app()->environment(['local', 'testing']) && in_array($status, [500, 503, 404, 403])) {
+                return Inertia::render('Error', ['status' => $status])
+                    ->toResponse($request)
+                    ->setStatusCode($status);
+            }
+
+            return $response;
+        });
     })->create();
