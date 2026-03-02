ecf0b19782933c785b2ed287c9b4652fc29c87fd
mthiagoalves <m.thiago.ales@live.com>
2026-03-02 12:18:45 -0300

    feat(BrandsTable): make a new table to save brands info

---
+++ database/migrations/2026_03_02_145111_create_brands_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+
+            $table->boolean('has_video')->default(false);
+            $table->string('subtitle')->nullable();
+            $table->text('description')->nullable();
+            $table->string('website', 500)->nullable();
+            $table->string('website_text')->nullable();
+            $table->json('socials')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brands');
+    }
+};
