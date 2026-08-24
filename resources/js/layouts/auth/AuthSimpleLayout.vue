e80cda4196fefba16ee306badf97094c0b884dc9
Thiago Alves <m.thiago.alves@live.com>
2026-08-24 09:55:10 -0300

    feat(style): customize app page login

--- resources/js/layouts/auth/AuthSimpleLayout.vue
+++ resources/js/layouts/auth/AuthSimpleLayout.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
-import { Link } from '@inertiajs/vue3';
 
 defineProps<{
     title?: string;
@@ -9,28 +8,20 @@ defineProps<{
 </script>
 
 <template>
-    <div
-        class="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10"
-    >
-        <div class="w-full max-w-sm">
+    <div class="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10 hero-banner">
+        <span
+            class="absolute top-0 left-0 w-full h-full bg-cover bg-center bg-[linear-gradient(195deg,#42424a_0%,#191919_100%)] opacity-80">
+        </span>
+        <div class="w-full max-w-sm z-10">
             <div class="flex flex-col gap-8">
                 <div class="flex flex-col items-center gap-4">
-                    <Link
-                        href="/"
-                        class="flex flex-col items-center gap-2 font-medium"
-                    >
-                        <div
-                            class="mb-1 flex h-9 w-9 items-center justify-center rounded-md"
-                        >
-                            <AppLogoIcon
-                                class="size-9 fill-current text-[var(--foreground)] dark:text-white"
-                            />
-                        </div>
-                        <span class="sr-only">{{ title }}</span>
-                    </Link>
+                    <div class="mb-1 flex h-3xs w-3xs items-center justify-center rounded-md">
+                        <AppLogoIcon class="size-9 fill-current text-[var(--foreground)] dark:text-white" />
+                    </div>
+                    <span class="sr-only">{{ title }}</span>
                     <div class="space-y-2 text-center">
-                        <h1 class="text-xl font-medium">{{ title }}</h1>
-                        <p class="text-center text-sm text-muted-foreground">
+                        <h1 class="text-xl font-medium text-white">{{ title }}</h1>
+                        <p class="text-center text-sm text-white">
                             {{ description }}
                         </p>
                     </div>
@@ -40,3 +31,12 @@ defineProps<{
         </div>
     </div>
 </template>
+
+<style scoped>
+.hero-banner {
+    min-height: calc(100svh - 72px);
+    background: url("/images/homepage/luxury-living-room-with-gold-chandelier-and-curvy-sofas.jpg");
+    background-size: cover;
+    background-position: center center;
+}
+</style>
--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -6,7 +6,6 @@ import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AuthBase from '@/layouts/AuthLayout.vue';
-import { register } from '@/routes';
 import { store } from '@/routes/login';
 import { request } from '@/routes/password';
 import { Form, Head } from '@inertiajs/vue3';
@@ -39,7 +38,7 @@ defineProps<{
             v-slot="{ errors, processing }"
             class="flex flex-col gap-6"
         >
-            <div class="grid gap-6">
+            <div class="grid gap-6 text-white">
                 <div class="grid gap-2">
                     <Label for="email">Email address</Label>
                     <Input
@@ -99,14 +98,6 @@ defineProps<{
                     />
                     Log in
                 </Button>
-            </div>
-
-            <div
-                class="text-center text-sm text-muted-foreground"
-                v-if="canRegister"
-            >
-                Don't have an account?
-                <TextLink :href="register()" :tabindex="5">Sign up</TextLink>
             </div>
         </Form>
     </AuthBase>
